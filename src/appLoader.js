a94da0158eb91bea1e15e13faa63247f5a88295e
coryell1287 <acousticamericana.@gmail.com>
2017-12-08 23:57:39 -0600

    Remove react-themeable and add sourcemaps for JSX.

--- src/appLoader.js
+++ src/appLoader.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { connectRouter } from 'connected-react-router';
 import { render } from 'react-dom';
-import ThemeProvider from 'styles/react-toolbox/lib/ThemeProvider';
+import ThemeProvider from 'styles/theme/lib/ThemeProvider';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 
+import { host } from 'api/serviceConfig';
 import { store, history } from 'store/configureStore';
 import ReactHelmet from 'containers/ReactHelmet';
 import Routes from 'routes';
 import rootReducer from 'reducers';
 import HotLoader from './reactHotLoader';
+
+axios.defaults.baseURL = host;
+axios.interceptors.request.use((request) => {
+  return request;
+});
 
 const renderUI = (App) => {
   render(
--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,13 +1,25 @@
 import React from 'react';
+
 import AppBar from 'styles/theme/lib/app_bar';
 import Navigation from 'styles/theme/lib/navigation';
 import Link from 'styles/theme/lib/link';
+import appStyles from 'styles/appStyles';
+
+const AccountCircle = () => (
+  <i className={appStyles['material-icons']}>account_circle</i>
+);
+const Logo = () => (
+  <i className={appStyles['material-icons']}>weekend</i>
+);
+const Email = () => (
+  <i className={appStyles['material-icons']}>email</i>
+);
 
 const AppHeader = () => (
-  <AppBar title="Dashboard" leftIcon="home" rightIcon={""}>
+  <AppBar title="Dashboard" leftIcon={<Logo />} rightIcon={<Logo />}>
     <Navigation type="horizontal">
-      <Link href="http://" label="Inbox" icon="inbox"/>
-      <Link href="http://" active label="Profile" icon="person"/>
+      <Link href="http://" label="Inbox" icon={<Email />} />
+      <Link href="http://" active label="Profile" icon={<AccountCircle />} />
     </Navigation>
   </AppBar>
 );
